c14a97bfbb187080cdea3cc0d83781fb87976a8e
Jason Lengstorf <jason@lengstorf.com>
2018-03-16 08:45:39 -0700

    refactor: simplify, clean up, add logging

--- js/photos.js
+++ js/photos.js
@@ -1,48 +1,120 @@
-const API_URL = 'https://pixabay.com/api/';
+/**
+ * Replaces the contents of a node matching the selector with the given markup.
+ *
+ * 🚨 DANGER: This function is impure! 🚨
+ *
+ * @param  {String} selector  query selector for the target DOM node
+ * @param  {String} markup    HTML markup to be inserted into the target node
+ * @return {void}
+ */
+const addMarkupToDOM = R.curry((selector, markup) => {
+  document.querySelector(selector).innerHTML = markup;
+});
 
-const getURL = (search, key) => {
-  const url = new URL(API_URL);
+const addToPhotoContainer = addMarkupToDOM('.photos');
+
+/**
+ * Retrieves an API key from the URL query string.
+ *
+ * 🚨 DANGER: This function is impure! 🚨
+ *
+ * @param  {String} search  search term to be passed through
+ * @return {Object}         unmodified search term and retrieved API key
+ */
+const getKeyFromURL = search => {
+  const key = new URL(window.location).searchParams.get('key');
+
+  if (!key) {
+    // If we didn’t find a key, log a helpful error, then die.
+    addToPhotoContainer(`
+      <p class="photos__error">
+        A key is required in the URL (e.g. ?key=MY_API_KEY). Get an API key
+        from <a href="https://pixabay.com/api/docs/">Pixabay</a>.
+      </p>
+    `);
+    throw new Error('Missing API key');
+  }
+
+  return { search, key };
+};
+
+/**
+ * Generates a URL to call the Pixabay API.
+ *
+ * @see https://pixabay.com/api/docs/
+ *
+ * @param  {String} search  search term to use on Pixabay
+ * @param  {String} key     API key for the Pixabay API
+ * @return {String}         full URL to query the Pixabay API
+ */
+const getSearchURL = ({ search, key }) => {
+  const url = new URL('https://pixabay.com/api/');
 
   url.searchParams.set('key', key);
+  url.searchParams.set('q', search);
+
+  // These settings make sure we only get high quality photos.
   url.searchParams.set('image_type', 'photo');
   url.searchParams.set('editors_choice', 'true');
-  url.searchParams.set('q', search);
 
   return url;
 };
 
-const getImageDetails = R.pipe(
-  R.prop('hits'),
-  R.map(
-    R.applySpec({
-      src: R.prop('webformatURL'),
-      alt: R.prop('tags'),
-      link: R.prop('pageURL')
-    })
-  ),
-  R.map(
-    R.evolve({
-      src: R.replace('_640', '_340'),
-      alt: R.identity,
-      link: R.identity
-    })
-  )
-);
+/**
+ * Converts a full Pixabay image object into a simplified object.
+ * @param  {Object} image   image object to simplify
+ * @return {Object}         image object with only required props
+ */
+const simplifyImageObject = R.applySpec({
+  src: R.prop('webformatURL'),
+  alt: R.prop('tags'),
+  link: R.prop('pageURL')
+});
 
-const convertToMarkup = ({ src, alt, link }) =>
-  `<a href="${link}" class="list__link"><img src="${src}" alt="${alt}" class="list__image" /></a>`;
+/**
+ * Replaces the size string in the image object to use a smaller size.
+ * @param  {Object} image   image object to transform
+ * @return {Object}         image object with smaller image size
+ */
+const transformImageSize = R.evolve({
+  src: R.replace('_640', '_340'),
+  alt: R.identity,
+  link: R.identity
+});
 
-const getMarkup = R.compose(R.join(''), R.map(convertToMarkup));
+/**
+ * Applies data transformations to an array of image objects.
+ * @param  {Array} images   array of image objects to be transformed
+ * @return {Array}          array of transformed image objects
+ */
+const getImageDetails = R.compose(transformImageSize, simplifyImageObject);
 
-const addToDOM = markup => {
-  document.querySelector('.list').innerHTML = markup;
-};
+const getImagesArray = R.compose(R.map(getImageDetails), R.prop('hits'));
+
+const convertToMarkup = ({ src, alt, link }) => `
+  <a href="${link}" class="photos__link">
+    <img src="${src}" alt="${alt}" class="photos__image" />
+  </a>
+`;
+
+const getImageMarkup = R.compose(R.join(''), R.map(convertToMarkup));
+
+const logAndReturn = R.tap(data => console.log(data));
 
 const fetchResults = url =>
   fetch(url)
     .then(body => body.json())
-    .then(getImageDetails)
-    .then(getMarkup)
-    .then(addToDOM);
+    .then(logAndReturn)
+    .then(getImagesArray)
+    .then(logAndReturn)
+    .then(getImageMarkup)
+    .then(logAndReturn)
+    .then(addToPhotoContainer);
 
-export default R.compose(fetchResults, getURL);
+export default R.compose(
+  fetchResults,
+  logAndReturn,
+  getSearchURL,
+  logAndReturn,
+  getKeyFromURL
+);
